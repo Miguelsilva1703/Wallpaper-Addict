674dd9135514b3b88265ddefa2a6fc29ab9e34ff
Miguel Silva <miguelsilva170393@gmail.com>
2022-10-05 19:19:54 +0100

    Structure, styling and basic API GET

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
 //PEXELS API KEY: 563492ad6f9170000100000182ae1445a87f4cc7ba0be08b25c7e1ec
 
+//API STUFF
+const auth = "563492ad6f9170000100000182ae1445a87f4cc7ba0be08b25c7e1ec";
+
+//Main Variables
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitBtn = document.querySelector(".submit-btn");
+const moreDiv = document.querySelector(".nav-btn");
+const moreBtn = document.querySelector(".more");
+let searchValue;
+
+
+async function curatedPhotos(){
+    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1",{
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            authorization: auth
+        } 
+    }
+    );
+    const data = await dataFetch.json();
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement("div");
+        galleryImg.classList.add("gallery-img");
+        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+        <p>${photo.photographer}</p>
+        `;
+        gallery.appendChild(galleryImg);
+    });
+}
+
+curatedPhotos();
+
+
+
