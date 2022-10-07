42185f1bb708d73c440c42b1616d5dfc20a9612b
Miguel Silva <miguelsilva170393@gmail.com>
2022-10-07 17:33:35 +0100

    API fetch and Data manip final

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ let searchValue;
 //Event Listeners
 searchInput.addEventListener("input", updateInput);
 form.addEventListener("submit", (e) => {
-    searchPhotos(searchValue);
     //This prevent form from refreshing on submit (need to passevent to anonym function)
-    e.preventDefault;
+    e.preventDefault();
+    searchPhotos(searchValue);
+    
 });
 
 function updateInput(e){
@@ -24,16 +25,19 @@ function updateInput(e){
 }
 
 
-async function curatedPhotos(){
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1",{
+async function fetchApi(url){
+    const dataFetch = await fetch(url, {
         method: "GET",
         headers: {
             Accept: "application/json",
-            authorization: auth
-        } 
-    }
-    );
+            Authorization: auth
+        }
+    });
     const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data){
     data.photos.forEach(photo => {
         const galleryImg = document.createElement("div");
         galleryImg.classList.add("gallery-img");
@@ -44,23 +48,19 @@ async function curatedPhotos(){
     });
 }
 
+
+
+
+async function curatedPhotos(){
+    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
+    generatePictures(data);
+    
+}
+
 async function searchPhotos(input){
-    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${searchValue}+query&per_page=15&page=1`, {
-        method: "GET",
-        headers:{
-            Accept: "application/json",
-            Authorization: auth
-        }
-    }
-    );
-    const data = await dataFetch.json();
-    data.photos.forEach(photo => {
-        const galleryImg = document.createElement("div");
-        galleryImg.classList.add("gallery-img");
-        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>`;
-        gallery.appendChild(galleryImg);
-    });
+    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${input}&page=1&per_page=15`)
+    generatePictures(data);
+    
 }
 
     
